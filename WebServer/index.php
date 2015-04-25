ff596bfce0faf34f5d23de4b11fb4e99b06f016b
Joe Cheatham <cheathamjoe@gmail.com>
2015-04-24 22:37:57 -0500

    Sample Queue Page
    Sample Queue Page Added

---
+++ WebServer/index.php
@@ -0,0 +1,61 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>MusicQ - Web Interface</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootplus.css" rel="stylesheet">
+    <link href="css/darkstrap.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-default">
+  	<div class="container-fluid">
+    	<div class="navbar-header">
+      		<a class="navbar-brand" href="#">MusicQ</a>
+    	</div>
+  	</div>
+</nav>
+
+<div class="container">
+  
+	<div class="text-center">
+    	<h1>MusicQ</h1>
+    	<p class="lead">All the music.</p>
+  	</div>
+
+ 	<div class="card">
+		<h3 class="card-heading simple">Song - Artist</h3>
+		<div class="card-body">
+		Submitted by...
+		</div>
+		<div class="card-comments">
+			<div class="comments-collapse-toggle">
+				<a data-toggle="collapse" data-target="#c1-comments" href="#c1-comments">3 Likes<i class="icon-angle-down"></i></a>
+			</div>
+	
+		</div>
+	</div>
+  
+</div><!-- /.container -->
+
+</body>
+
+</html>
